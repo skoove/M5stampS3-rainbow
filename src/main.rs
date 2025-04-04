4c33689ba70bc2fadf5a7e5b4fc2d3c5715721d5
skoove <zie@skoove.dev>
2025-04-04 21:52:49 +1000

    simple blinky

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_svc::hal::prelude::*;
+use ws2812_esp32_rmt_driver::Ws2812Esp32RmtDriver;
 
-    // Bind the log crate to the ESP Logging facilities
+fn main() {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap();
+    let led_pin = peripherals.pins.gpio21;
+    let channel = peripherals.rmt.channel0;
+    let mut driver = Ws2812Esp32RmtDriver::new(channel, led_pin).unwrap();
+    let colors: [[u8; 3]; 4] = [
+        [255, 255, 255],
+        [255, 000, 000],
+        [000, 255, 000],
+        [000, 000, 255],
+    ];
+
     loop {
-        log::info!("Hello, world!");
+        for color in &colors {
+            let led_data: [u8; 3] = *color;
+            std::thread::sleep(std::time::Duration::from_millis(500));
+            driver.write_blocking(led_data.into_iter()).unwrap();
+        }
     }
 }
